d29aea5408b1ff7e53fdd21cf168cacfa5d32551
Rasmus Olsen <69420936+RasLillebo@users.noreply.github.com>
2020-08-29 13:08:33 +0200

    Update RecommendationAlgorithm.R

--- RecommendationAlgorithm.R
+++ RecommendationAlgorithm.R
@@ -1,12 +1,18 @@
 #Generalized recommendation engine based on User ranking, (and further user analysis)
+#  Engines like these are not only employed by frontend of ecommerce, but can also be used in mapping store/warehouse layout: such that items are placed closer together,
+#  when bought more often bought or at the same time. It can also be employed in loyalty Programmes:To reactivate churned costumers by re-engaging them with customised offers 
+#  based on their purchase history.
+
 #Load Packages
-Packages <- c("data.table", "tidyverse", "knitr", "recommenderlab", "stringi", "reshape2", "dplyr", "rts", "plyr")
+Packages <- c("data.table", "tidyverse", "knitr", "recommenderlab", 
+              "stringi", "reshape2", "dplyr", "rts", "plyr")
+#install.packages(Packages)
 lapply(Packages, library, character.only=TRUE)
 
 #Data: UserID, itemID, rating, timestamp
-Data=read.csv("C:/Users/PC/Desktop/ratings.csv") #UserID, itemID, rating, timestamp
+Data=read.csv("C:/Users/rasmu/OneDrive/Skrivebord/Github/Data/ratings.csv") #UserID (int), itemID (int), rating (num), timestamp (int)
 names(Data) <- c("UserID", "ItemID", "Rating", "Timestamp")
-Dim_Data = read.csv("C:/Users/PC/Desktop/dimension.csv") #ItemID, ProductName, ProductCategory
+Dim_Data = read.csv("C:/Users/rasmu/OneDrive/Skrivebord/Github/Data/dimension.csv") #ItemID (int), ProductName (chr), ProductCategory (chr)
 names(Dim_Data) <- c("ItemID", "ProductName", "ProductCategory")
 
 #Create ratings matrix with rows as users and columns as movies. We don't need timestamp
@@ -25,16 +31,18 @@ k <- 20 #Number of steps in progress bar
 pbar <- create_progress_bar('text')
 pbar$init(k)
 for (i in 1:length(Data$UserID)) { #For all UserID's, estimate recommendations using nearest neighbour specified above
-Top_5_pred = predict(modRating, mRating[i], n=5)# Obtain top 5 recommendations for X'st userID entry in dataset
-Top_5_List = as(Top_5_pred, "list") #Convert the recommendations to a list
-Top_5_df=data.frame(Top_5_List) #We convert the list to a dataframe and change the column name to ItemID
-colnames(Top_5_df)="ItemID" #Chaning the name of the vector
-Top_5_df$ItemID=as.numeric(levels(Top_5_df$ItemID))#Since ItemID is of type integer in Dim_Data, we typecast id in our recommendations as well
-
-names=left_join(Top_5_df, Dim_Data, by="ItemID")# Merge the ItemID with names to get ProductName and ProductCategory
-RecList[[i]] <- names
-pbar$step()
+  Top_5_pred = predict(modRating, mRating[i], n=5)# Obtain top 5 recommendations for X'st userID entry in dataset
+  Top_5_List = as(Top_5_pred, "list") #Convert the recommendations to a list
+  Top_5_df=data.frame(Top_5_List) #We convert the list to a dataframe and change the column name to ItemID
+  colnames(Top_5_df)="ItemID" #Chaning the name of the vector
+  Top_5_df$ItemID=as.numeric((Top_5_df$ItemID))#Since ItemID is of type integer in Dim_Data, we typecast id in our recommendations as well
+  
+  names=left_join(Top_5_df, Dim_Data, by="ItemID")# Merge the ItemID with names to get ProductName and ProductCategory
+  RecList[[i]] <- names
+  pbar$step()
 }
+#What is recommended for UserID entry 1?
+RecList[[1]]
 
 #What recommendations are more present than others?
 Category <- 0
@@ -42,14 +50,7 @@ CatTable <- 0
 for (i in 1:length(RecList)) {
   Category = RecList[[i]]$ItemID
   CatTable = rbind(CatTable, Category)
-  }
-TopCategories <- sort(table(CatTable),decreasing=TRUE)[1:10]
-List <- as.numeric(names(TopCategories))
-
-
-#Work In progress: Trying to connect the ItemID's stored in TopCategories, with the Categories of Dim_Data.
-TopCategories <- which(apply(Dim_Data, 1, function(List) any(List %in% Dim_Data)))
-
-#What UserID's are ranking higher than others?
-
-
+}
+TopCategories <- names(sort(table(CatTable),decreasing=TRUE)[1:10])
+TopCategories <- Dim_Data[Dim_Data$ItemID %in% TopCategories,]
+TopCategories
